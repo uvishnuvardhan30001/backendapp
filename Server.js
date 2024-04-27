4a804c8e5ee691a5b9dbf8ef7175cfc32f6d655a
Uppuluri Vishnu Vardhan <vishnuvardhan.uppuluri@gmail.com>
2024-04-27 08:34:48 +0530

    env

--- Server.js
+++ Server.js
@@ -2,7 +2,7 @@ const express = require("express")
 const mongoose = require("mongoose")
 const cors = require("cors")
 
-const dburl = "mongodb://localhost:27017/musicapi"
+const dburl = process.env.mongourl
 mongoose.connect(dburl).then(() => {
     console.log("Connected to DB Successfully")
 }).catch((err) => {
